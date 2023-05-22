0007f5cc2f4ef31f580ff6e1e5de29c5f693a13c
Girish Kumar <girishkumarsp70@gmail.com>
2023-05-22 17:59:54 +0530

    meta tags

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      this is footer
+      @Copyright Littel Lemon
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div >
+      <img src="https://www.48hourslogo.com/oss/works/2022/01/12/135523929088/115168_45900_d4d2e8ab-0a96-4bc4-ae9d-f9898dc70956.jpg" alt="little lemon" height={70} width={140} />
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Famous</a></li>
+            <li><a href="/">About</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
