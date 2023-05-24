cb06da521eece056332fdd2153a7d5980bd2991b
Girish Kumar <girishkumarsp70@gmail.com>
2023-05-24 20:49:43 +0530

    navbar done

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./Main.css"
 
 function Main() {
   return (
-    <div>
+    <div className='container'>
       Main component
     </div>
   )
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import "./Nav.css"
 
 function Nav() {
   return (
-    <div >
-      <img src="https://www.48hourslogo.com/oss/works/2022/01/12/135523929088/115168_45900_d4d2e8ab-0a96-4bc4-ae9d-f9898dc70956.jpg" alt="little lemon" height={70} width={140} />
-        <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Famous</a></li>
-            <li><a href="/">About</a></li>
-        </ul>
+    <div className='navbar'>
+      <img src={require('./assets/Logo.svg').default} alt="little lemon" height={70} width={140} />
+      <ul className='list'>
+        <li><a href="/">Home</a></li>
+        <li><a href="/">About</a></li>
+        <li><a href="/">Reservation</a></li>
+        <li><a href="/">Order</a></li>
+        <li><a href="/">Login</a></li>
+
+      </ul>
     </div>
   )
 }
